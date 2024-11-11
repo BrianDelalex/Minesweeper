6ffa37cf81d20fedecb78963e81cefd7e2fabc61
BrianDelalex <brian.delalex@outlook.com>
2024-11-11 14:51:09 +0100

    Rendering using addchar (was echochar) to make the rendering smooth. Adding rendering char depending on cell values.

--- src/game/render/mod.rs
+++ src/game/render/mod.rs
@@ -1,14 +1,36 @@
 use crate::game::Game;
 
-use crate::ncurses::{clearw, echochar, printw, set_cursor_pos};
+use crate::ncurses::{
+    addchar, attroff, attron, clearw, echochar, printw, refreshw, set_cursor_pos, COLOR_PAIR,
+    PAIR_BLUE_WHITE,
+};
+
+use super::Cell;
 
 pub fn render(game: &Game) {
     clearw();
     for y in 0..((game.height) as usize) {
         for x in 0..((game.width - 1) as usize) {
-            echochar(game.map[y * (game.width as usize) + x].content as i8);
-            echochar(32);
+            let cell = game.map[y * (game.width as usize) + x];
+            if cell.debug_color != 0 {
+                attron(COLOR_PAIR(cell.debug_color));
+            }
+            if cell.hidden {
+                if cell.flagged {
+                    addchar('?' as i8);
+                } else {
+                    addchar('#' as i8);
+                }
+            } else {
+                addchar(cell.content as i8);
+            }
+            addchar(32);
+            if cell.debug_color != 0 {
+                attroff(COLOR_PAIR(cell.debug_color));
+            }
         }
-        echochar('\n' as i8);
+
+        addchar('\n' as i8);
     }
+    refreshw();
 }
